6344449ed83e3d2aeba23688c8c9f05272788ce6
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-05-16 09:49:01 +0300

    allow to run cli by python interpreter

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from typing import Annotated
 
-from typer import Argument
+from typer import Argument, run
 
 
 def main(name: Annotated[str, Argument()] = "World"):
     print(f"Hello {name}")
+
+
+if __name__ == "__main__":
+    run(main)
