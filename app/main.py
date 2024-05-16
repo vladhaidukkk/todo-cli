440a8f1f3d4ee57fb2a171cc89c05a2d3ea985fc
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-05-16 09:57:46 +0300

    replace the single entry function with typer app

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from typing import Annotated
 
-from typer import Argument, run
+from typer import Argument, Typer
+
+app = Typer()
 
 
+@app.command()
 def main(name: Annotated[str, Argument()] = "World"):
     print(f"Hello {name}")
 
 
 if __name__ == "__main__":
-    run(main)
+    app()
