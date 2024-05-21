7e7460925f4de7400582b2d0f69e12b364df85cd
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-05-21 23:27:27 +0300

    create local db for unix based systems

--- todo/main.py
+++ todo/main.py
@@ -1,8 +1,23 @@
+import os
+from pathlib import Path
 from typing import Annotated
 
 from typer import Argument, Typer
 
-app = Typer()
+app = Typer(no_args_is_help=True)
+
+
+def get_data_dir() -> Path:
+    data_home = os.environ.get("XDG_DATA_HOME", "~/.local/share")
+    return Path(f"{data_home}/todo").expanduser()
+
+
+@app.callback()
+def setup():
+    data_dir = get_data_dir()
+    data_dir.mkdir(parents=True, exist_ok=True)
+    db_file = data_dir / "todo.db"
+    db_file.touch(exist_ok=True)
 
 
 @app.command()
