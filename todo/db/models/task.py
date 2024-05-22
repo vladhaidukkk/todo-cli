199d7871410e168f2cde447f53ae6ca8ab62e063
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-05-22 13:04:22 +0300

    create task model

---
+++ todo/db/models/task.py
@@ -0,0 +1,22 @@
+from datetime import date, datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
+
+from todo.db.core import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    target_date: Mapped[date | None]
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.current_timestamp()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.current_timestamp(),
+        onupdate=datetime.now(),
+    )
+    completed_at: Mapped[datetime | None]
