105c96bcb6af1dbf81c9bdc794d856836efe4587
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-05-25 13:25:20 +0300

    create add command for tasks creation

--- todo/main.py
+++ todo/main.py
@@ -1,6 +1,7 @@
-from typing import Annotated
+from datetime import datetime
+from typing import Annotated, Optional
 
-from typer import Argument, Typer
+from typer import Argument, BadParameter, Option, Typer
 
 from todo.config import settings
 from todo.db.management import apply_migrations
@@ -9,12 +10,31 @@ app = Typer(no_args_is_help=True)
 
 
 @app.callback()
-def setup():
+def setup() -> None:
     settings.data_dir.mkdir(parents=True, exist_ok=True)
     settings.db_file.touch(exist_ok=True)
     apply_migrations()
 
 
+def title_callback(value: str) -> str:
+    if not value.strip():
+        raise BadParameter("Only non-empty string is allowed.")
+    return value
+
+
 @app.command()
-def main(name: Annotated[str, Argument()] = "World"):
-    print(f"Hello {name}")
+def add(
+    title: Annotated[str, Argument(callback=title_callback, show_default=False)],
+    target_date: Annotated[
+        Optional[datetime],
+        Option(
+            "--date",
+            "-d",
+            formats=["%Y-%m-%d"],
+            metavar="YYYY-MM-DD",
+            prompt="Date",
+            prompt_required=False,
+        ),
+    ] = None,
+) -> None:
+    print(title, target_date and target_date.date())
