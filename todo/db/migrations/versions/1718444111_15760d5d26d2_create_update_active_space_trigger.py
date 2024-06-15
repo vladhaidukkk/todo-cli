f16220d118fcae33ce5820576ae868e0f6be040b
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-06-15 12:43:26 +0300

    create trigger to update spaces active column

---
+++ todo/db/migrations/versions/1718444111_15760d5d26d2_create_update_active_space_trigger.py
@@ -0,0 +1,34 @@
+"""Create update_active_space trigger.
+
+Revision ID: 15760d5d26d2
+Revises: ca66626ab021
+Create Date: 2024-06-15 09:35:11.526030+00:00
+
+"""
+
+from collections.abc import Sequence
+from typing import Union
+
+from alembic import op
+
+revision: str = "15760d5d26d2"
+down_revision: Union[str, None] = "ca66626ab021"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute(
+        """
+        CREATE TRIGGER update_active_space
+        BEFORE UPDATE ON spaces
+        WHEN NEW.active = TRUE AND OLD.active = FALSE
+        BEGIN
+            UPDATE spaces SET active = FALSE WHERE id != NEW.id AND active = TRUE;
+        END;
+        """
+    )
+
+
+def downgrade() -> None:
+    op.execute("DROP TRIGGER update_active_space;")
